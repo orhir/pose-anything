84f5e2008766d9006278562002e91e8fe7a650de
ajevnisek <ajevnisek@gmail.com>
2023-11-20 16:23:20 +0200

    init

--- static/js/index.js
+++ static/js/index.js
@@ -43,22 +43,22 @@ $(document).ready(function () {
         breakpoint: 1024,
         settings: {
           arrows: true,
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         }
       }
     ]
