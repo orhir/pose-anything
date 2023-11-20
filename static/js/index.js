b2efcb90199b5a3806b020c5066387e41c0d39f3
ajevnisek <ajevnisek@gmail.com>
2023-11-20 11:16:54 +0200

    init

---
+++ static/js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  $('.results-carousel').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+    $('.ood-carousel').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+})
